ee6d23f774f6c3b31c3a1130fa629b38be70bd87
Luiggi <luiggi.ms.1@gmail.com>
2024-03-29 23:31:34 -0300

    Deleted Files

--- src/components/Button copy.js
+++
@@ -1,5 +0,0 @@
-import {Button} from "@mui/material";
-
-export default function ButtonComponent({text}){
-    return <Button variant="text" sx={{marginTop:1.4, fontSize: 14, fontWeight:"bold"}}>{text}</Button>
-}
