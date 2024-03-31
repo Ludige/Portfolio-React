2489e1f4de31b31438e452060c4b8ae6b471d87b
Luiggi <luiggi.ms.1@gmail.com>
2024-03-31 16:50:40 -0300

    Commit: Animated Icons to Component, and Improvements to Responsivity of the Screen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { Box, AppBar, Container, Paper, Typography, Divider, IconButton} from "@mui/material";
+import { Box, AppBar, Container, Typography, Divider, IconButton, Grid} from "@mui/material";
 import { Component } from "react";
 import Lottie from "lottie-react";
 import 'aos/dist/aos.css';
@@ -15,11 +15,7 @@ import computingCat from "./assets/icons/progrsmadorGato.json";
 //Icons
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import { RiFlutterFill } from "react-icons/ri";
-import { FaJava } from "react-icons/fa";
-import { SiJavascript } from "react-icons/si";
-import { FaNodeJs,FaPython,FaReact } from "react-icons/fa";
-import { DiDart,DiMysql } from "react-icons/di";
+import AnimatedIcons from "./components/AnimatedIcons";
 
 // function scrollTo (){
 //     Link.scrollTo('contato');
@@ -52,84 +48,75 @@ class App extends Component{
           flexDirection: "row",
         }}>
           <Box sx={{pt:38,px:3}}>
-            <Lottie animationData={sleepingCatAnimation} style={{width:"40vw"}}/>
+            <Lottie animationData={sleepingCatAnimation} style={{width:"40vw", height:"80vh"}}/>
           </Box>
           <Box sx={{pt: 25}}>
             <Presentation></Presentation>
           </Box>
         </Container>
-       
       </Box>
       <Container sx={{height: "500vh"}}>
-            <Paper elevation={0} sx={{paddingTop:"10vh", display: "flex", flexDirection: "row"}} >
-              <Box sx={{width: {md:"34vw"}, height: {md:"36vh"}, marginLeft:"20vh"}} data-aos="fade-right">
-                <Typography  sx={{
-                  fontSize:32,
-                  justifyContent: "center",
-                  textAlign:"center",
-                  fontWeight:"bold", color:"purple",
-                  paddingBottom:2,
-                  position:"relative"
-                }} variant="h2" >
-                  Sobre mim
-                </Typography>
-                <Typography sx={{
-                  fontSize:16,
-                  textAlign:"justify",
-                  position:"relative"
-                  }} >
-                  Meu nome é Luiggi, tenho {getAge()} anos, Sou desenvolvedor FullStack, minha jornada na programação
-                  começou em 2019 desde então adquiri conhecimento em diversas linguagens, nelas inclusas Java,
-                  JavaScript, HTML+CSS, Dart, Python, NodeJs, Sql, noSQL. Além de alguns frameworks como o React e Flutter.
-                </Typography>
-                <Box sx={{display: "flex",justifyContent:"center", marginTop:2}}>
-                  <IconButton sx={{color:"purple", marginRight:2}} onClick={() =>{window.open("https://github.com/ludige", "_blank")}}>
-                    <GitHubIcon sx={{width:36, height:36}} />
-                  </IconButton>
-                  <IconButton sx={{color:"purple"}}>
-                    <LinkedInIcon sx={{width:36, height:36}} onClick={() =>{window.open("https://www.linkedin.com/in/ludige/", "_blank")}}/>
-                  </IconButton>
-                </Box>
+          <Box sx={{paddingTop:8, display: "flex",flexDirection: "row"}}  >
+            <Box sx={{width: 400, height:200, marginLeft:20}} data-aos="fade-right">
+              {/* Text */}
+              <Typography  sx={{
+                fontSize:32,
+                justifyContent: "center",
+                textAlign:"center",
+                fontWeight:"bold", color:"purple",
+                paddingBottom:2,
+                position:"relative",
+                fontFamily:"Atlan",
+                overflow: 'hidden',
+                textOverflow:"ellipsis",
+                height:30
+              }} variant="h2" >
+                Sobre mim
+              </Typography>
+              <Typography sx={{
+                fontSize:16,
+                textAlign:"justify",
+                position:"relative",
+                maxWidth:"34vw",
+                maxHeight:"30vh",
+                fontFamily:"Atlan",
+                fontWeight:"bold",
+                overflow: 'hidden',
+                textOverflow:"ellipsis",
+                height:150
+                }}>
+                Meu nome é Luiggi, tenho {getAge()} anos, Sou desenvolvedor FullStack, minha jornada na programação
+                começou em 2019 desde então adquiri conhecimento em diversas linguagens, nelas inclusas Java,
+                JavaScript, HTML+CSS, Dart, Python, NodeJs, Sql, noSQL. Além de alguns frameworks como o React e Flutter.
+              </Typography>
+              {/* Buttons */}
+              <Box sx={{display: "flex",justifyContent:"center", marginTop:2}}>
+                <IconButton sx={{color:"purple", marginRight:2}} onClick={() =>{window.open("https://github.com/ludige", "_blank")}}>
+                  <GitHubIcon sx={{width:36, height:36}} />
+                </IconButton>
+                <IconButton sx={{color:"purple"}}>
+                  <LinkedInIcon sx={{width:36, height:36}} onClick={() =>{window.open("https://www.linkedin.com/in/ludige/", "_blank")}}/>
+                </IconButton>
               </Box>
-              <Box sx={{width: {xs:1, md:"34vw"}, height: {xs:1, md:"36vh"}}}>
-                <Lottie animationData={computingCat} style={{width:"26vw", marginLeft: 48}} data-aos="fade-left"/>
-              </Box>
-            </Paper>
-
-            <Divider sx={{marginTop:5,marginBottom:5 ,color:"purple"}} data-aos="zoom-in" data-aos-delay="200"/>
-
-            <Box sx={{display: "flex",justifyContent:"center"}}>
-              <Paper sx={{width:"36vw",display: "flex",justifyContent:"center"}} elevation={0}>
-                <Box sx={{marginRight:2}} data-aos="fade-up" data-aos-duration="600" data-aos-delay="100">
-                  <RiFlutterFill  color="purple" size={50}/>
-                </Box>
-                <Box sx={{marginRight:2}} data-aos="fade-down" data-aos-duration="600" data-aos-delay="200">
-                  <FaJava color="purple" size={50}/>
-                </Box>
-                <Box sx={{marginRight:2}} data-aos="fade-up" data-aos-duration="600" data-aos-delay="300">
-                  <SiJavascript color="purple" size={50} />
-                </Box>
-                <Box sx={{marginRight:2}} data-aos="fade-down" data-aos-duration="600" data-aos-delay="400">
-                  <FaNodeJs color="purple" size={50}/>
-                </Box>
-                <Box sx={{marginRight:2}} data-aos="fade-up" data-aos-duration="600" data-aos-delay="500">
-                  <FaPython color="purple" size={50}/>
-                </Box>
-                <Box sx={{marginRight:2}} data-aos="fade-down" data-aos-duration="600" data-aos-delay="600">
-                  <FaReact color="purple" size={50}/>
-                </Box>
-                <Box sx={{marginRight:2}} data-aos="fade-up" data-aos-duration="600" data-aos-delay="700">
-                  <DiDart color="purple" size={50}/>
-                </Box>
-                <Box sx={{marginRight:2}} data-aos="fade-down" data-aos-duration="600" data-aos-delay="800">
-                  <DiMysql color="purple" size={50}/>
-                </Box>
-              </Paper>
             </Box>
+            {/* Animation */}
+            <Box sx={{width:"34vw", height: "36vh"}} xs={2} > 
+              <Lottie animationData={computingCat} style={{width:"26vw"}} data-aos="fade-left" />
+            </Box>
+          </Box>
 
-            <Divider sx={{marginTop:5,marginBottom:5 ,color:"purple"}} data-aos="zoom-in" data-aos-delay="200"/>
+          <Divider sx={{marginTop:5,marginBottom:5 ,color:"purple"}} data-aos="zoom-in" data-aos-delay="200"/>
 
+          <Box sx={{display: "flex",justifyContent:"center"}}>
+            <AnimatedIcons />
+          </Box>
+
+          <Divider sx={{marginTop:5,marginBottom:5 ,color:"purple"}} data-aos="zoom-in" data-aos-delay="200"/>
+
+          <Grid>
             <ProjectSection />
+          </Grid>
+
       </Container>
     </Box>
     );
---
+++ src/components/AnimatedIcons.js
@@ -0,0 +1,35 @@
+import {Box, Paper} from "@mui/material";
+import { RiFlutterFill } from "react-icons/ri";
+import { FaJava } from "react-icons/fa";
+import { SiJavascript } from "react-icons/si";
+import { FaNodeJs,FaPython,FaReact } from "react-icons/fa";
+import { DiDart,DiMysql } from "react-icons/di";
+
+export default function AnimatedIcons(){
+    return <Paper sx={{width:"36vw",display: "flex",justifyContent:"center"}} elevation={0}>
+    <Box sx={{marginRight:2}} data-aos="fade-up" data-aos-duration="600" data-aos-delay="100">
+      <RiFlutterFill  color="purple" size={50}/>
+    </Box>
+    <Box sx={{marginRight:2}} data-aos="fade-down" data-aos-duration="600" data-aos-delay="200">
+      <FaJava color="purple" size={50}/>
+    </Box>
+    <Box sx={{marginRight:2}} data-aos="fade-up" data-aos-duration="600" data-aos-delay="300">
+      <SiJavascript color="purple" size={50} />
+    </Box>
+    <Box sx={{marginRight:2}} data-aos="fade-down" data-aos-duration="600" data-aos-delay="400">
+      <FaNodeJs color="purple" size={50}/>
+    </Box>
+    <Box sx={{marginRight:2}} data-aos="fade-up" data-aos-duration="600" data-aos-delay="500">
+      <FaPython color="purple" size={50}/>
+    </Box>
+    <Box sx={{marginRight:2}} data-aos="fade-down" data-aos-duration="600" data-aos-delay="600">
+      <FaReact color="purple" size={50}/>
+    </Box>
+    <Box sx={{marginRight:2}} data-aos="fade-up" data-aos-duration="600" data-aos-delay="700">
+      <DiDart color="purple" size={50}/>
+    </Box>
+    <Box sx={{marginRight:2}} data-aos="fade-down" data-aos-duration="600" data-aos-delay="800">
+      <DiMysql color="purple" size={50}/>
+    </Box>
+  </Paper>
+}
